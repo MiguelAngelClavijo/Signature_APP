454060e6bea57de8430e265cc4953877dd08655f
Miguel Angel Clavijo Sanchez <miguel.clavijos@colvatel.com>
2023-03-15 11:33:19 -0500

    [FIX] Icons tel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,11 @@ const Header = () => {
     <div className="header">
       <img src={logo} alt="icon" />
       <h1>
-        Complete los datos a continuación para previsualizar su firma digital
-        para copiarla haga clic en el botón <span>"Copiar Firma"</span>. Luego puede insertarla en su correo. Si cuenta con número de celular
-        corporativo seleccione <span>"Firma 1"</span> y si no cuenta con número
-        de celular corporativo seleccione <span>"Firma 2"</span>
+        Complete los datos a continuación para previsualizar su firma digital.
+        Para copiarla y usarla en su correo corporativo haga clic en el botón{" "}
+        <span>"Copiar Firma"</span>. Luego puede insertarla en su correo. Si
+        cuenta con número de celular corporativo seleccione{" "}
+        <span>"Firma 1"</span> y si no seleccione <span>"Firma 2"</span>
       </h1>
     </div>
   );
