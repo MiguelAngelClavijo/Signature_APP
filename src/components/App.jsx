faba4bffa98db57db92a715a9553fd6f66c03e93
unknown <sergio.gomez2000@hotmail.com>
2023-03-14 23:23:05 -0500

    [ADD] custom hooks, and functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "@styles/global.scss";
 import { Header } from "./Header";
 import { Utilities } from "./Utilities";
+import { Footer } from "../containers/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Utilities />
+      <Footer/>
     </div>
   );
 };
